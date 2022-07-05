72a49891966348abe502f0e35588da86b8180dd0
Luca Pugliese <lucapug@outlook.it>
2022-07-05 20:02:00 +0200

    if else preprocess sys.argv

--- data_preprocessing/parse_json_lite.py
+++ data_preprocessing/parse_json_lite.py
@@ -38,12 +38,16 @@ import emoji
 
 fieldsFilter = fields.fields
 
-fileN = sys.argv[1]
+#fileN = sys.argv[1]
 #preprocess = sys.argv[2] #gives Error if second argument 'p' is omitted in the call
 
+fileN = sys.argv[0]
+
 #preprocess = sys.argv[1]
-#if len(sys.argv) > 1:
-#    preprocess = sys.argv[2]
+if len(sys.argv) > 1:
+    preprocess = sys.argv[2]
+else:
+   preprocess = ''
    
 data = []
 
